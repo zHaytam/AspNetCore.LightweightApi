be7cd2b130aadf1d85f4e7a637851d94725a9b12
zHaytam <haytam.zanid@gmail.com>
2020-11-30 21:20:13 +0100

    feat: Helpful Get extension method for route values

--- samples/SampleAPI/Endpoints/Users/UpdateUserHandler.cs
+++ samples/SampleAPI/Endpoints/Users/UpdateUserHandler.cs
@@ -1,14 +1,16 @@
 ﻿using AspNetCore.LightweightApi;
+using AspNetCore.LightweightApi.Extensions;
 using Microsoft.AspNetCore.Http;
 using System.Threading.Tasks;
 
 namespace SampleAPI.Endpoints.Users
 {
-    [Endpoint("/users", Method = EndpointMethod.Patch)]
+    [Endpoint("/users/{id}", Method = EndpointMethod.Patch)]
     public class UpdateUserHandler : IEndpointHandler.IWithRequest<UpdateUserDto>
     {
         public Task Handle(UpdateUserDto request, HttpContext context)
         {
+            var id = context.Request.RouteValues.Get<int>("id");
             // Update in db
             return Task.CompletedTask;
         }
---
+++ src/AspNetCore.LightweightApi/Extensions/RouteValueDictionaryExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Routing;
+using System;
+
+namespace AspNetCore.LightweightApi.Extensions
+{
+    public static class RouteValueDictionaryExtensions
+    {
+        public static T? Get<T>(this RouteValueDictionary routeValues, string key)
+        {
+            var value = routeValues[key];
+            if (value == null)
+                return default;
+
+            var t = typeof(T);
+            var u = Nullable.GetUnderlyingType(t);
+            return (T)Convert.ChangeType(value, u ?? t);
+        }
+    }
+}
