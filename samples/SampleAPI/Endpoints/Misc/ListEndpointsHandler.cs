e5644a44c5cabe604991ce970c7f901e95a6dca0
zHaytam <haytam.zanid@gmail.com>
2020-12-01 11:22:15 +0100

    docs: add handler with dependencies example

---
+++ samples/SampleAPI/Endpoints/Misc/ListEndpointsHandler.cs
@@ -0,0 +1,27 @@
+﻿using AspNetCore.LightweightApi;
+using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleAPI.Endpoints.Misc
+{
+    [Endpoint("endpoints")]
+    public class ListEndpointsHandler : IEndpointHandler.IWithResponse<IEnumerable<EndpointEntry>>
+    {
+        private readonly EndpointCollection _endpointCollection;
+
+        public ListEndpointsHandler(EndpointCollection endpointCollection)
+        {
+            _endpointCollection = endpointCollection;
+        }
+
+        public Task<IEnumerable<EndpointEntry>> Handle(HttpContext context)
+        {
+            var endpoints = _endpointCollection.Items.Select(i => new EndpointEntry(i.Method.ToString(), i.Pattern));
+            return Task.FromResult(endpoints);
+        }
+    }
+
+    public record EndpointEntry(string Method, string Pattern);
+}
