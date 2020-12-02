fbbe6d888c349af83cf4a3a73a951a7425471a35
zHaytam <haytam.zanid@gmail.com>
2020-12-02 15:09:09 +0100

    docs: Auth sample project

---
+++ samples/Auth/Endpoints/HelloWorldHandler.cs
@@ -0,0 +1,16 @@
+﻿using AspNetCore.LightweightApi;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace Auth.Endpoints
+{
+    [Authorize]
+    public class HelloWorldHandler : IEndpointHandler
+    {
+        public async Task Handle(HttpContext context)
+        {
+            await context.Response.WriteAsync("Hello World!");
+        }
+    }
+}
---
+++ samples/Auth/Startup.cs
@@ -0,0 +1,58 @@
+using AspNetCore.LightweightApi.Extensions;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace Auth
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.UseLightweightApi();
+
+            services.AddAuthorization();
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.SaveToken = true;
+                options.RequireHttpsMetadata = false;
+                options.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidAudience = "JWT:ValidAudience",
+                    ValidIssuer = "JWT:ValidIssuer",
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("Secret"))
+                };
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.UseLightweightApi();
+            });
+        }
+    }
+}
