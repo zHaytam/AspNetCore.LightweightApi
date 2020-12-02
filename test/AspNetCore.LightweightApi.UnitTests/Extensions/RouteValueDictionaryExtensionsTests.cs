fe2de70268d0c97658c72c4ca29195908897d4bf
zHaytam <haytam.zanid@gmail.com>
2020-12-02 19:46:02 +0100

    test: add UnitTests projects

---
+++ test/AspNetCore.LightweightApi.UnitTests/Extensions/RouteValueDictionaryExtensionsTests.cs
@@ -0,0 +1,22 @@
+﻿using AspNetCore.LightweightApi.Extensions;
+using Microsoft.AspNetCore.Routing;
+using Xunit;
+
+namespace AspNetCore.LightweightApi.UnitTests.Extensions
+{
+    public class RouteValueDictionaryExtensionsTests
+    {
+        [Fact]
+        public void Get_ShouldReturnNull_WhenKeyDoesntExist()
+        {
+            // Arrange
+            var routeValues = new RouteValueDictionary();
+
+            // Act
+            var value = routeValues.Get<string>("key");
+
+            // Assert
+            Assert.Null(value);
+        }
+    }
+}
