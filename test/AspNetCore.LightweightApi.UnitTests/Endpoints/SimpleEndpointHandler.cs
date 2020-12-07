ba18ead7b93eb0d8f7ff927059fee9c8dbcb9faf
zHaytam <haytam.zanid@gmail.com>
2020-12-07 13:45:06 +0100

    tests: ServiceCollectionExtensions

---
+++ test/AspNetCore.LightweightApi.UnitTests/Endpoints/SimpleEndpointHandler.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Threading.Tasks;
+
+namespace AspNetCore.LightweightApi.UnitTests.Endpoints
+{
+    public class SimpleEndpointHandler : IEndpointHandler
+    {
+        public Task Handle(HttpContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ test/AspNetCore.LightweightApi.UnitTests/Extensions/ServiceCollectionExtensionsTests.cs
@@ -0,0 +1,38 @@
+﻿using AspNetCore.LightweightApi.Extensions;
+using AspNetCore.LightweightApi.UnitTests.Endpoints;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace AspNetCore.LightweightApi.UnitTests.Extensions
+{
+    public class ServiceCollectionExtensionsTests
+    {
+        [Fact]
+        public void UseLightweightApi_ShouldRegisterEndpointsAsScoped()
+        {
+            // Arrange
+            var serviceCollection = new ServiceCollection();
+
+            // Act
+            serviceCollection.UseLightweightApi();
+
+            // Assert
+            var entry = serviceCollection[0];
+            Assert.Equal(typeof(SimpleEndpointHandler), entry.ImplementationType);
+            Assert.Equal(ServiceLifetime.Scoped, entry.Lifetime);
+        }
+
+        [Fact]
+        public void UseLightweightApi_ShouldRegisterCollection()
+        {
+            // Arrange
+            var serviceCollection = new ServiceCollection();
+
+            // Act
+            serviceCollection.UseLightweightApi();
+
+            // Assert
+            Assert.Contains(serviceCollection, s => s.ServiceType == typeof(EndpointCollection));
+        }
+    }
+}
